3073924ceb15689320848c50ee18420641c954d8
Ben Foster <7606550+fostrb@users.noreply.github.com>
2019-12-10 15:53:43 -0500

    Create scrapman.py

---
+++ services/scrapman.py
@@ -0,0 +1,33 @@
+import threading
+import time
+import subprocess
+import socket
+import argparse
+import ConfigParser
+from sserv import SServ
+
+import os
+
+
+class ScreencapMan(SServ):
+    """demo service"""
+    def __init__(self):
+        super(ScreencapMan, self).__init__()
+        self.heartbeat = 60
+        print("ScreencapMan Init")
+        print("Heartbeat: " + str(self.heartbeat))
+    
+    def start_server(self):
+        pass
+
+    # exposed commands ---
+    def cmd_capture_at(self, freq):
+        self.heartbeat = int(freq)
+        print("Heartbeat: " + str(self.heartbeat))
+
+    # exposed  -------|
+    
+    
+    def cleanup(self):
+        pass
+    
